8cc7475f566050876c412259c6c29ca24f81091d
Madison McGaffin <greyhill@gmail.com>
2014-01-30 13:13:55 -0500

    plots for cpu and gpu transistor counts

--- mkplots.py
+++ mkplots.py
@@ -12,11 +12,42 @@ for f in glob.glob('data/*.dat'):
             years.append(float(s[0]))
             speeds.append(float(s[1]))
 
-pl.plot(years, speeds, 'o', alpha=.8, markeredgewidth=0, label='Intel CPUs')
+pl.scatter(years, speeds, marker='o', linewidth=0, alpha=.8, label='Intel CPUs')
 pl.ylabel('Clock Speed [Hz]')
 pl.xlabel('Year')
 pl.axis([None, None, -50, None])
 pl.title('Intel CPU Clock Speeds')
 pl.show()
 pl.savefig('intel_clock_speeds.pdf')
+
+def parse_xistor_year_speed(path):
+    years = []
+    counts = []
+    for line in fileinput.input(path):
+        s = line.split('\t')
+        if len(s) >= 3:
+            count = ''.join(s[1].split(','))
+            year = s[2]
+            years.append(float(year))
+            counts.append(float(count))
+            if count <= 0:
+                print s
+    return years, counts
+
+cpu_xistor = parse_xistor_year_speed('data/cpu_transistor.txt')
+gpu_xistor = parse_xistor_year_speed('data/gpu_transistor.txt')
+
+pl.figure()
+pl.scatter(*cpu_xistor, label='CPU', marker='o', alpha=.8, 
+        color='blue', linewidth=0)
+pl.scatter(*gpu_xistor, label='GPU', marker='o', alpha=.8, 
+        color='green', linewidth=0)
+pl.axis([-1 + min(cpu_xistor[0] + gpu_xistor[0]), 
+    1 + max(cpu_xistor[0] + gpu_xistor[0]), -.1e9, None])
+pl.xlabel('Year')
+pl.ylabel('Transistor count')
+pl.legend(loc='upper left')
+pl.title('CPU and GPU Transistor Counts')
+pl.show()
+pl.savefig('transistor_count.pdf')
 
