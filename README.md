f74cac126ce30394971cacb5f6010b46e2f366b4
Madison McGaffin <greyhill@gmail.com>
2014-01-30 12:18:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cpuclocks
+
+A bunch of CPU clock speed information, from cgillespie's [article][0] on
+r-bloggers.
+
+[0]: http://www.r-bloggers.com/cpu-and-gpu-trends-over-time/ "CPU and GPU trends"
+
---
+++ mkplots.py
@@ -0,0 +1,22 @@
+import fileinput
+import glob
+import pylab as pl
+
+years = []
+speeds = []
+
+for f in glob.glob('data/*.dat'):
+    for line in fileinput.input(f):
+        s = line.split(' ')
+        if len(s) == 2 and s[0] != '#':
+            years.append(float(s[0]))
+            speeds.append(float(s[1]))
+
+pl.plot(years, speeds, 'o', alpha=.8, markeredgewidth=0, label='Intel CPUs')
+pl.ylabel('Clock Speed [Hz]')
+pl.xlabel('Year')
+pl.axis([None, None, -50, None])
+pl.title('Intel CPU Clock Speed')
+pl.show()
+pl.savefig('intel_clock_speeds.pdf')
+
